ef2aa8136ed5601183785c5a3fe55e14b0e7fe56
EhiElohor <okoruwaehinomen2017@gmail.com>
2022-10-18 12:32:45 +0100

    notification page

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+let all = document.querySelectorAll("main div");
+let dots = document.querySelectorAll("span.dot");
+document.querySelector(".toggle").addEventListener("click", () =>{
+    document.querySelector(".fig").innerHTML = "0";
+    for (a of all) {
+        a.style.backgroundColor = "white";
+        a.querySelector("div p span.dot").style.display = "none";
+    }
+})
+
+function changeNum() {
+    if (document.querySelector (".fig").innerHTML > 0) {
+        document.querySelector(".fig").innerHTML = 
+        document.querySelector(".fig").innerHTML - 1;   }
+    else{
+        document.querySelector(".fig").innerHTML = 0;
+    }
+}
+
+function bgChange(b) {
+    document.getElementById(b).style.backgroundColor = "white";
+}
+
+function displayChange(c) {
+    document.querySelector(c).style.display = "none";
+}
+
+for (a of all) {
+    let didi = a.id;
+    a.addEventListener("click", () =>{
+        bgChange(didi);
+        displayChange("#" + didi + " div p span.dot");
+        changeNum();
+    }, { once: true})
+    
+}
